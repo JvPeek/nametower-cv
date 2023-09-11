f5386e722fd186ffc94fa99209d10ae6cb70385f
Jan <janofthings@gmail.com>
2023-09-11 17:30:01 +0200

    MQTT output

--- livecapture2.py
+++ livecapture2.py
@@ -3,12 +3,21 @@ import os
 import numpy as np
 import argparse
 import time
+import json
+import paho.mqtt.client as mqtt
 
 # Function to reload template files
 def reload_templates():
     global template_files
     template_files = [f for f in os.listdir(templates_folder) if f.lower().endswith(".jpg")]
     print("Reloaded template files")
+
+# Function to send JSON data to MQTT broker
+def send_to_mqtt(data):
+    client = mqtt.Client()
+    client.connect("192.168.2.11", 1883)
+    client.publish("nametower/namelist", json.dumps(data))
+    client.disconnect()
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Template Matching with Configurable Threshold")
@@ -46,26 +55,18 @@ newImage = False
 capture_start = None
 capture_end = None
 
-# Initialize capture marker
-capture_marker = None
-
 # Reload templates
 reload_templates()
 
 def capture_region(event, x, y, flags, param):
-    global capturing, capture_start, capture_end, newImage, capture_marker
+    global capturing, capture_start, capture_end, newImage
     if capturing:
         if event == cv2.EVENT_LBUTTONDOWN:
             capture_start = (x, y)
-            capture_marker = (x, y)
-        elif event == cv2.EVENT_MOUSEMOVE and capture_start:
-            # Update the capture marker coordinates
-            capture_marker = (x, y)
         elif event == cv2.EVENT_LBUTTONUP:
             newImage = True
             capturing = False
             capture_end = (x, y)
-            capture_marker = None
 
 cv2.namedWindow('frame')
 cv2.setMouseCallback('frame', capture_region)
@@ -76,6 +77,10 @@ while True:
     # Draw a capturing mode indicator
     if capturing:
         cv2.putText(frame, "Capturing Mode ON", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        if capture_start and capture_end:
+            x1, y1 = capture_start
+            x2, y2 = capture_end
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
     # If in detection mode, perform template matching
     if detecting:
@@ -105,28 +110,23 @@ while True:
                     best_matches[template_name] = (max_val, max_loc)
                     
                     # Store the coordinates of the detected region and its name
-                    detected_regions.append((max_loc, (max_loc[0] + template.shape[1], max_loc[1] + template.shape[0]), template_name))
+                    detected_regions.append({
+                        "start": [max_loc[0], max_loc[1]],
+                        "end": [max_loc[0] + template.shape[1], max_loc[1] + template.shape[0]],
+                        "name": template_name
+                    })
 
-    if capture_start and capture_end:
-        x1, y1 = capture_start
-        x2, y2 = capture_end
-        captured_region = frame[min(y1, y2):max(y1, y2), min(x1, x2):max(x1, x2)]
+        # Sort detected regions by their y-coordinate
+        detected_regions.sort(key=lambda x: x["start"][1])
 
-        # Check if the captured region is not empty before saving it
-        if not captured_region.size == 0:
-            # Generate a unique filename (e.g., based on a timestamp)
-            timestamp = str(int(time.time()))
-            capture_filename = os.path.join(templates_folder, f"captured_{timestamp}.jpg")
-            
-            # Save the captured region as a JPG image
-            cv2.imwrite(capture_filename, captured_region)
-            
-            print(f"Saved captured region as {capture_filename}")
+        # Create a JSON object with the sorted data
+        sorted_data = {
+            "detected_regions": detected_regions,
+            "timestamp": int(time.time())
+        }
 
-            # Reset capturing mode
-            capturing = False
-            capture_start = None
-            capture_end = None
+        # Send JSON data to MQTT broker
+        send_to_mqtt(sorted_data)
 
     if displayMarkers:
         # Draw rectangles and labels for the best matches and previously detected regions
@@ -136,12 +136,6 @@ while True:
             bottom_right = (top_left[0] + template.shape[1], top_left[1] + template.shape[0])
             cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0), 2)
             cv2.putText(frame, template_name, (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
-
-    # If in capture mode, draw the capture box
-    if capturing and capture_start and capture_marker:
-        x1, y1 = capture_start
-        x2, y2 = capture_marker
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
